231e01cf82914094beffcb33d5fb6eb11daa7257
snowman2 <alansnow21@gmail.com>
2020-07-16 20:28:00 -0500

    pyproj 3 & PROJ 7.1

--- config.sh
+++ config.sh
@@ -1,52 +1,37 @@
 # Define custom utilities
 # Test for macOS with [ -n "$IS_OSX" ]
-PROJ_VERSION=7.0.1
-DATUMGRID_VERSION=1.8
-SQLITE_VERSION=3310100
+PROJ_VERSION=7.1.0
+SQLITE_VERSION=3320300
 LIBTIFF_VERSION=4.1.0
-CURL_VERSION=7.69.1
+CURL_VERSION=7.71.1
 
 export PROJ_WHEEL=true
 
+
 function build_curl {
-    if [ -e curl-stamp ]; then return; fi
-    fetch_unpack https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
-    (cd curl-${CURL_VERSION} \
-        && ./configure --prefix=$BUILD_PREFIX \
-        && make -j4 \
-        && make install)
-    touch curl-stamp
+    build_simple curl $CURL_VERSION https://curl.haxx.se/download
 }
 
 function build_libtiff {
-    if [ -e libtiff-stamp ]; then return; fi
-    fetch_unpack https://download.osgeo.org/libtiff/tiff-${LIBTIFF_VERSION}.tar.gz
-    (cd tiff-${LIBTIFF_VERSION} \
-        && ./configure --prefix=$BUILD_PREFIX \
-        && make -j4 \
-        && make install)
-    touch libtiff-stamp
+    build_simple tiff $LIBTIFF_VERSION https://download.osgeo.org/libtiff
 }
 
 function build_sqlite {
     if [ -e sqlite-stamp ]; then return; fi
-    fetch_unpack https://www.sqlite.org/2020/sqlite-autoconf-${SQLITE_VERSION}.tar.gz
-    (cd sqlite-autoconf-${SQLITE_VERSION} \
-        && ./configure --prefix=$BUILD_PREFIX \
-        && make -j4 \
-        && make install)
+    if [ -n "$IS_OSX" ]; then
+        brew install sqlite3
+        sqlite3 --version
+    else
+        build_simple sqlite-autoconf $SQLITE_VERSION https://www.sqlite.org/2020
+    fi
     touch sqlite-stamp
 }
 
 function build_proj {
     if [ -e proj-stamp ]; then return; fi
-    fetch_unpack http://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
-    (cd proj-${PROJ_VERSION:0:5}/data \
-        && curl http://download.osgeo.org/proj/proj-datumgrid-${DATUMGRID_VERSION}.zip > proj-datumgrid.zip \
-        && unzip -o proj-datumgrid.zip \
-        && rm proj-datumgrid.zip \
-        && cd .. \
-        && ./configure --prefix=$PROJ_DIR \
+    # fetch_unpack https://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
+    (cd proj-${PROJ_VERSION:0:5} \
+        && ./configure --prefix=$PROJ_DIR --with-curl=${BUILD_PREFIX}/bin/curl-config \
         && make -j4 \
         && make install)
     touch proj-stamp
@@ -58,6 +43,8 @@ function pre_build {
     suppress build_zlib
     suppress build_sqlite
     suppress build_libtiff
+    # unpack early because SSL support not added with custom curl
+    fetch_unpack https://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
     suppress build_curl
     export PROJ_DIR=$PWD/pyproj/pyproj/proj_dir
     build_proj
@@ -68,6 +55,10 @@ function pre_build {
 }
 
 function run_tests {
+    pyproj -v
+    pyproj sync --file us_noaa_alaska.tif --verbose
+    pyproj sync --file us_noaa_emhpgn.tif --verbose
+    pyproj sync --file us_noaa_conus.tif --verbose
     python -m pip install shapely || echo "Shapely install failed"
     # Runs tests on installed distribution from an empty directory
     python --version
