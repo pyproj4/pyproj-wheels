05e705bbb4ff904a8bee81eb7e94590764e3e4b8
Alan Snow <alan.snow@corteva.com>
2022-03-16 20:34:38 -0500

    CI: Test cibuildwheel

---
+++ .github/workflows/install_proj.sh
@@ -0,0 +1,25 @@
+echo "::group::Install PROJ"
+  source multibuild/common_utils.sh
+  source multibuild/travis_steps.sh
+  # https://github.com/multi-build/multibuild/pull/451
+  function clean_code {
+      local repo_dir=${1:-$REPO_DIR}
+      local build_commit=${2:-$BUILD_COMMIT}
+      [ -z "$repo_dir" ] && echo "repo_dir not defined" && exit 1
+      [ -z "$build_commit" ] && echo "build_commit not defined" && exit 1
+      # The package $repo_dir may be a submodule. git submodules do not
+      # have a .git directory. If $repo_dir is copied around, tools like
+      # Versioneer which require that it be a git repository are unable
+      # to determine the version.  Give submodule proper git directory
+      fill_submodule "$repo_dir"
+      (cd $repo_dir \
+          && git fetch origin --tags\
+          && git checkout $build_commit \
+          && git clean -fxd \
+          && git reset --hard \
+          && git submodule update --init --recursive)
+  }
+  clean_code $REPO_DIR $BUILD_COMMIT
+  source config.sh
+  pre_build
+echo "::endgroup::"
